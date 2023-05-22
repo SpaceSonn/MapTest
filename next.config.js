6a01caf7c464ecc5671f95047863667b374bf450
Amartuvshin Batdelger <115851089+SpaceSonn@users.noreply.github.com>
2023-05-22 15:26:51 +0800

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env:{
-        NEXT_PUBLIC_MAP_TOKEN:"AIzaSyDZ6Xy-3qMPCtvqigbxd4kjWsnf2m2tVOg"
-    }
-}
-
-module.exports = nextConfig
