56d5dbc66cf1e09b3754d2b441b09bcc24caeb1e
SpaceSonn <amkaamka436@yahoo.com>
2023-05-22 15:18:18 +0800

    added env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env:{
+        NEXT_PUBLIC_MAP_TOKEN:"AIzaSyDZ6Xy-3qMPCtvqigbxd4kjWsnf2m2tVOg"
+    }
+}
 
 module.exports = nextConfig
